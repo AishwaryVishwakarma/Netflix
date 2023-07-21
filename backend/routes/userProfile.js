beea4d74743957f185c383e07881b486f20c8cff
Titan1911 <sahilahuja1911@gmail.com>
2023-07-22 00:46:24 +0530

    resolved deleteProfile not found bug

--- backend/routes/userProfile.js
+++ backend/routes/userProfile.js
@@ -95,6 +95,11 @@ router.delete('/delete-profile/:user_profile_id', userMiddleware.authenticateJWT
 
         const deleteProfile = userProfile.profiles.id(profile._id)
 
+        if ( deleteProfile == null ){
+            res.status(404).send({"detail":"Profile Not Found"})
+            return 
+        }
+
         if ( deleteProfile.meta.deletable === false ){
             res.status(406).send({"detail": "Cannot delete this profile"})
             return 
@@ -103,7 +108,7 @@ router.delete('/delete-profile/:user_profile_id', userMiddleware.authenticateJWT
         deleteProfile.deleteOne()
         await userProfile.save()
         
-        res.status(204) // accepted but nothing to return
+        res.status(200).send({"detail":"Successful"}) // accepted but nothing to return
 
     }
     catch(err){
