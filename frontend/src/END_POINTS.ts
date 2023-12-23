22f77800f4151dc3c0a2605c7382c35b35995481
AishwaryVishwakarma <aishwaryvishwakarma23@gmail.com>
2023-12-23 21:33:02 +0530

    feat: wiring up Fe with BE

--- frontend/src/END_POINTS.ts
+++ frontend/src/END_POINTS.ts
@@ -1,17 +1,15 @@
-const port = 8000;
+export const login = `${process.env.NEXT_PUBLIC_HOST}login`; // post / get
 
-export const login = `http://localhost:${port}/login`;  // post / get
+export const signup = `${process.env.NEXT_PUBLIC_HOST}signup`; // post
 
-export const signup = `http://localhost:${port}/signup`;  // post
+export const checkUser = `${process.env.NEXT_PUBLIC_HOST}checkuser`; // post
 
-export const checkUser = `http://localhost:${port}/checkuser`;  // post
+export const setSubscription = `${process.env.NEXT_PUBLIC_HOST}set-subscription`; // post
 
-export const setSubscription = `http://localhost:${port}/set-subscription`; // post
+export const userProfile = `${process.env.NEXT_PUBLIC_HOST}profiles/`; //post (need user id at the end)
 
-export const userProfile = `http://localhost:${port}/profiles/`;  //post (need user id at the end)
+export const createProfile = `${process.env.NEXT_PUBLIC_HOST}create-profile`; // post
 
-export const createProfile = `http://localhost:${port}/create-profile`; // post
+export const deleteProfile = `${process.env.NEXT_PUBLIC_HOST}delete-profile/`; // delete (need user id at the end)
 
-export const deleteProfile = `http://localhost:${port}/delete-profile/`;  // delete (need user id at the end)
-
-export const updateProfile = `http://localhost:${port}/update-profile/`;  // put (need user id at the end)
+export const updateProfile = `${process.env.NEXT_PUBLIC_HOST}update-profile/`; // put (need user id at the end)
