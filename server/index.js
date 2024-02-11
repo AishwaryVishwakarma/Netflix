a3b680f74006f75845aef97c3a9eb223b6cd46ef
Aishwary Vishwakarma <aishwaryvishwakarma23@gmail.com>
2024-02-11 18:49:48 +0530

    fix: added credentials to cors

--- server/index.js
+++ server/index.js
@@ -31,16 +31,17 @@ var allowedOrigins = [
 app.use(
   cors({
     origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
+      // allow requests with no origin (like mobile apps or curl requests)
       // if(!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
+        console.log('if block');
         var msg =
           'The CORS policy for this site does not allow access from the specified Origin.';
         return callback(new Error(msg), false);
       }
       return callback(null, true);
     },
+    credentials: true,
   })
 );
 
